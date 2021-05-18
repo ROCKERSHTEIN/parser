a448cfad30abd77db18c3d22c87cffdfe265d492
Vladislav Smirnov <vladislav.smirn2012@gmail.com>
2021-05-18 14:24:11 +0300

    Добавление файла проекта

---
+++ main.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://духи.рф/catalog/women'
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'lxml')
+quotes = soup.find_all('a', class_='items')
+
+for quote in quotes:
+    print(quote.text)
